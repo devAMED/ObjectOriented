ce23f1d9cd8289d20f213e5bb7b55279ae4db26c
devAMED <syntaggo@gmail.com>
2025-03-27 10:02:20 +0400

    Create A21.java

---
+++ Quiz1/src/muhammad_ahmed_1/quiz1/t2/knife/A21.java
@@ -0,0 +1,22 @@
+package muhammad_ahmed_1.quiz1.t2.knife;
+
+public class A21 {
+    private int width = 23;
+    private int height = 37;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
