779874cb47638eba29ad6b8b3dab780db9961fbc
devAMED <syntaggo@gmail.com>
2025-03-27 10:03:54 +0400

    Create A22.java

---
+++ Quiz1/src/muhammad_ahmed_1/quiz1/t2/knife/vinegar/opera/A22.java
@@ -0,0 +1,15 @@
+package muhammad_ahmed_1.quiz1.t2.vinegar.opera;
+
+public class A22 {
+    private int month = 7;
+
+
+    public int getMonth() {
+        return month;
+    }
+
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+}
