b78ffdcf18894453304233cd4bb84ff0708ceb9e
devAMED <syntaggo@gmail.com>
2025-03-27 07:47:57 +0400

    Assignment01

---
+++ lecture1quiz/Assignment01.java
@@ -0,0 +1,16 @@
+package objectoriented.lecture1quiz;
+
+
+/**
+ * Correct formatting, naming conventions and the code itself
+ * 1 point
+ */
+class Assignment01 {
+    public static void main(String[] args) {
+
+        int a = 17;
+        int b = 18;
+
+        System.out.println("The sum of the numbers is" + (a + b));
+    }
+}
