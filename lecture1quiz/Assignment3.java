983bf3030080cbff37aacade4c34289850b512ae
devAMED <syntaggo@gmail.com>
2025-03-27 07:49:20 +0400

    Assignment3

---
+++ lecture1quiz/Assignment3.java
@@ -0,0 +1,16 @@
+package objectoriented.lecture1quiz;
+
+
+/**
+ * Find the file "hidden.txt" and print its path.
+ * The file should be found yourself without java code.
+ * 0.5 points
+ */
+public class Assignment03 {
+    public static void main(String[] args) {
+        String filepath="/Users/ahmed/Downloads/oop2025-main/workshop/w1/muhammad_ahmed_1/bird/charcoal/coupe/hidden.txt";
+        System.out.println("hidden.txt path file is : ");
+        System.out.println(filepath);
+    }
+
+}
