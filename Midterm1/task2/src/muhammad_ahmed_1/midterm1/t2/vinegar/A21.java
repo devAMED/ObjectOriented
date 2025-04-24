c930a1d64e0a3a94308f70f813ef3a430a154b07
devAMED <syntaggo@gmail.com>
2025-04-24 10:09:01 +0400

    Create A21.java

---
+++ Midterm1/task2/src/muhammad_ahmed_1/midterm1/t2/vinegar/A21.java
@@ -0,0 +1,22 @@
+package muhammad_ahmed_1.midterm1.t2.vinegar;
+
+public class A21 {
+    private int width = 23;
+    private int height = 37;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
