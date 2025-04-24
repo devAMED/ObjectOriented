ce119b1d602e9f697d4442fb057fa7c842d005fb
devAMED <syntaggo@gmail.com>
2025-04-24 10:05:48 +0400

    Create A22.java

---
+++ Midterm1/task2/src/muhammad_ahmed_1/midterm1/t2/knife/opera/A22.java
@@ -0,0 +1,10 @@
+package muhammad_ahmed_1.midterm1.t2.knife.opera;
+
+public class A22 {
+    private int month = 7;
+
+    // add this getter so R21 can read the month
+    public int getMonth() {
+        return month;
+    }
+}
